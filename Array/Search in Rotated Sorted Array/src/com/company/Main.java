a08f2fe5930998e9e1a8d2ac3a684f73fda07aec
nnatevan <natavann19509@sabah.edu.az>
2021-06-26 23:27:30 +0400

    Search in Rotated Sorted Arra y

---
+++ Array/Search in Rotated Sorted Array/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+    public int search(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        int[] nums = new int[]{1, 2, 3, 4, 6, 7};
+        System.out.println(main.search(nums, 7));
+    }
+
+}
